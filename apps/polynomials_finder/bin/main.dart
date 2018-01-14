d5111cbd1cea913a131cc4190cdd5111bcefff46
Alexei Diaz <alexei.eleusis@gmail.com>
2018-01-14 10:25:36 -0800

    Reducing the search space to only monic polynomials.

--- apps/polynomials_finder/bin/main.dart
+++ apps/polynomials_finder/bin/main.dart
@@ -2,25 +2,23 @@ import 'package:library/library.dart';
 
 void main(List<String> arguments) {
   final degree = 10;
-  primesLowerThan(15).forEach((characteristic) {
+  primesLowerThan(15).where((p) => p > 2).forEach((characteristic) {
     final polynomials = buildAllSequences(characteristic, degree + 1)
         .skip(1)
         .map((scalars) => scalars
-        .toList()
-        .reversed
-        .skipWhile((s) => s == 0)
-        .toList()
-        .reversed
-        .toList())
+            .toList()
+            .reversed
+            .skipWhile((s) => s == 0)
+            .toList()
+            .reversed
+            .toList())
         .where((s) => 2 <= s.length)
-        .map((s) =>
-        s.map((v) => new PrimeFiniteFieldScalar(v, characteristic)))
+        .map((s) => s.map((v) => new PrimeFiniteFieldScalar(v, characteristic)))
         .map((s) => new Polynomial(s))
-        .toList();
-    final irreducibles = polynomials
-        .where((p) => p.degree > 0)
         .where((p) => p.isMonic)
-        .toSet();
+        .toList();
+    final irreducibles =
+        polynomials.where((p) => p.degree > 0).where((p) => p.isMonic).toSet();
     polynomials.forEach((left) {
       polynomials.forEach((right) {
         irreducibles.remove(left * right);
